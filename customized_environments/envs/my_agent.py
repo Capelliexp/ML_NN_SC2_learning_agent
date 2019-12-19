10f92b9832a8d6dadfef1d858a1e89eea94fed6b
Filip <the.1man@hotmail.com>
2019-12-19 18:41:35 +0100

    change features and action space

--- customized_environments/envs/my_agent.py
+++ customized_environments/envs/my_agent.py
@@ -12,6 +12,8 @@ import logging
 import math
 import numpy as np
 
+FEATURE_SIZES = 16
+
 class CustomAgent(gym.Env):
     metadata = {'render.modes':['human']}
 
@@ -23,7 +25,7 @@ class CustomAgent(gym.Env):
             sc2_env.Bot(sc2_env.Race.zerg, sc2_env.Difficulty.hard)],
         'agent_interface_format': features.AgentInterfaceFormat(
             #feature_dimensions = None,
-            feature_dimensions = features.Dimensions(screen=64, minimap=64),
+            feature_dimensions = features.Dimensions(screen=FEATURE_SIZES, minimap=FEATURE_SIZES),
             rgb_dimensions = None,
             #raw_resolution = 100, #låt den vara standard
             action_space = actions.ActionSpace.RAW,
@@ -56,7 +58,7 @@ class CustomAgent(gym.Env):
         'visualize': False,
         #'visualize': True,
         #'step_mul': None,
-        'step_mul': 4,
+        'step_mul': 16,
         'realtime': False,    #should be false during training
         'save_replay_episodes': 0,
         'replay_dir': None,
@@ -82,20 +84,21 @@ class CustomAgent(gym.Env):
         self.banelings = []
         self.zerglings = []
 
-        self.selected = []
+        self.selected = {}
+        self.toward_enemy = [0,0]
 
         self.goal = [0,0]
 
-        self.observation_space = spaces.Box(low = 0, high = 255, shape = (64*2, 64, 1), dtype = np.float32)
+        self.observation_space = spaces.Box(low = 0, high = 255, shape = (FEATURE_SIZES + 1, FEATURE_SIZES, 1), dtype = np.float32)
 
         self.action_type = ""
 
         try:
             if all_args['learn_type'] == "DQN":
-                self.action_space = spaces.Discrete(9)
+                self.action_space = spaces.Discrete(12)
                 self.action_type = "Discrete"
             elif all_args['learn_type'] == "PPO2":
-                self.action_space = spaces.Box(np.array([-1, -1, 0]), np.array([1, 1, 1]))
+                self.action_space = spaces.Box(np.array([-1, -1, 0, 0, 0]), np.array([1, 1, 1, 1, 1]))
                 self.action_type = "Box"
         except:
             self.action_space = spaces.Box(np.array([-1, -1, 0]), np.array([1, 1, 1]))
@@ -137,32 +140,72 @@ class CustomAgent(gym.Env):
         else:
             self.current_marine_it = 0
 
-        """
-        obs = np.concatenate((
-            raw_obs.observation["feature_screen"][features.SCREEN_FEATURES.player_relative.index],
-            raw_obs.observation["feature_screen"][features.SCREEN_FEATURES.selected.index],
-            raw_obs.observation["feature_screen"][features.SCREEN_FEATURES.height_map.index]
-            ))
-        """
-
-        """
-        obs = np.concatenate((
-            raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.player_relative.index],
-            raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.selected.index],
-            raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.height_map.index]
-            ))
-        """
-
         rel = raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.player_relative.index]
         sel = raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.selected.index]
+        rel_sel = np.add(rel, sel)
+        rel_sel[rel_sel == 1] = 64
+        rel_sel[rel_sel == 2] = 128
+        rel_sel[rel_sel == 4] = 255
 
-        obs = np.concatenate(( np.add(rel, sel),
-            raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.height_map.index]
-            ))
+        hei = raw_obs.observation["feature_minimap"][features.MINIMAP_FEATURES.height_map.index]
+        hei[hei > 0] = 32
 
-        obs.resize((64*2, 64, 1))
+        obs = np.add(rel_sel, hei)
 
+        #obs.resize((FEATURE_SIZES + 1, FEATURE_SIZES, 1))
+        obs = np.resize(obs, (FEATURE_SIZES + 1, FEATURE_SIZES, 1))
+
+        if self.steps > 0:
+            rels = [0, 0, 0, 0]
+
+            enemy_dist = 255
+            for zerg in self.zerglings:
+                dist = math.sqrt((self.selected.x - zerg.x)**2 + (self.selected.y - zerg.y)**2)
+                if dist < enemy_dist:
+                    enemy_dist = dist
+                    rels[0] = (self.selected.x - zerg.x)
+                    rels[1] = (self.selected.y - zerg.y)
+            for bane in self.banelings:
+                dist = math.sqrt((self.selected.x - bane.x)**2 + (self.selected.y - bane.y)**2)
+                if dist < enemy_dist:
+                    enemy_dist = dist
+                    rels[0] = (self.selected.x - zerg.x)
+                    rels[1] = (self.selected.y - zerg.y)
+
+            friend_dist = 255
+            for friend in self.marines:
+                if friend.tag != self.selected.tag:
+                    dist = math.sqrt((self.selected.x - friend.x)**2 + (self.selected.y - friend.y)**2)
+                    if dist < friend_dist:
+                        friend_dist = dist
+                        rels[2] = (self.selected.x - zerg.x)
+                        rels[3] = (self.selected.y - zerg.y)
+            
+            """
+            for i in range(len(rels)):
+                if rels[i] < 0:
+                    rels[i] = 0
+                elif rels[i] > 0:
+                    rels[i] = 255
+                else:
+                    rels[i] = 128
+            """
+            self.toward_enemy = [rels[0], rels[1]]
+            rels = (np.sign(rels) + 1) * 127
+
+            tensor_count = 9
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(0/tensor_count))][0] = self.selected.health * 5 # HP
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(1/tensor_count))][0] = len(self.marines) * 12 # allies
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(2/tensor_count))][0] = (len(self.zerglings) + len(self.banelings)) * 7 # enemies
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(3/tensor_count))][0] = self.selected.weapon_cooldown * 255 # attack_reset
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(4/tensor_count))][0] = np.amin(enemy_dist) * 4 # distance to closest enemy
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(5/tensor_count))][0] = rels[0] # x towards enemy
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(6/tensor_count))][0] = rels[1] # y towards enemy
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(7/tensor_count))][0] = np.amin(friend_dist) # distance to closest friend
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(8/tensor_count))][0] = rels[2] # x towards friend
+            obs[FEATURE_SIZES][int((FEATURE_SIZES-1)*(9/tensor_count))][0] = rels[3] # y towards friend
         
+        """
         np.set_printoptions(threshold=np.inf)
 
         if self.episodes == 1 and self.steps == 1:
@@ -184,7 +227,8 @@ class CustomAgent(gym.Env):
         if self.episodes == 1 and self.steps == 5:
             print("obs")
             print(obs)
-        
+        """
+
         return obs
 
     def get_units_by_type(self, obs, unit_type, player_relative=0):
@@ -203,54 +247,66 @@ class CustomAgent(gym.Env):
 
     def take_action(self, action):
         # map value to an action
-
         action_mapped = []
+        new_pos = [0,0]
+
+        if len(self.marines) == 0:
+            raw_obs = self.env.step([action_mapped])[0]
+            return raw_obs
 
         if self.action_type == "Box":
             x = 1 if action[0] > 0 else -1
             y = 1 if action[1] > 0 else -1
             attack = action[2]
+            go_toward_enemy = action[3]
+            go_away_from_enemy = action[4]
 
             if self.steps > 1:
-                if attack > 0:
-                    action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, [self.selected.x, self.selected.y]))
+                if go_toward_enemy > 0.5:
+                    new_pos = [self.selected.x + self.toward_enemy[0], self.selected.y + self.toward_enemy[1]]
+                elif go_away_from_enemy > 0.5:
+                    new_pos = [self.selected.x - self.toward_enemy[0], self.selected.y - self.toward_enemy[1]]
                 else:
                     new_pos = [self.selected.x + x*2, self.selected.y + y*2]
-                    action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, new_pos))
 
-            self.selected = self.marines[self.current_marine_it]
-            action_mapped.append(actions.RAW_FUNCTIONS.Behavior_HoldFireOff_quick("now", self.selected.tag))
+                if attack > 0:
+                    action_mapped.append(actions.RAW_FUNCTIONS.Attack_pt("now", self.selected.tag, new_pos))
+                else:
+                    action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, new_pos))
         
         elif self.action_type == "Discrete":
             if self.steps > 1:
                 if action < 8:
-                    if action == 0:
+                    if action == 0 or action == 4:      # right
                         new_pos = [self.selected.x + 2, self.selected.y]
-                    elif action == 1:
-                        new_pos = [self.selected.x + 2, self.selected.y + 2]
-                    elif action == 2:
+                    elif action == 1 or action == 5:    # down
                         new_pos = [self.selected.x, self.selected.y + 2]
-                    elif action == 3:
-                        new_pos = [self.selected.x - 2, self.selected.y + 2]
-                    elif action == 4:
+                    elif action == 2 or action == 6:    # left
                         new_pos = [self.selected.x - 2, self.selected.y]
-                    elif action == 5:
-                        new_pos = [self.selected.x - 2, self.selected.y - 2]
-                    elif action == 6:
+                    elif action == 3 or action == 7:    # up
                         new_pos = [self.selected.x, self.selected.y - 2]
-                    elif action == 7:
-                        new_pos = [self.selected.x + 2, self.selected.y - 2]
-                    
-                    action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, new_pos))
+
+                    if action < 4:  # move in direction
+                        action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, new_pos))
+                    else:           # attack in direction
+                        action_mapped.append(actions.RAW_FUNCTIONS.Attack_pt("now", self.selected.tag, new_pos))
                 
                 else:
-                    action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, [self.selected.x, self.selected.y]))
-
-            self.selected = self.marines[self.current_marine_it]
-            action_mapped.append(actions.RAW_FUNCTIONS.Behavior_HoldFireOff_quick("now", self.selected.tag))
+                    if action == 8 or action == 10:     # toward enemy
+                        new_pos = [self.selected.x + self.toward_enemy[0], self.selected.y + self.toward_enemy[1]]
+                    elif action == 9 or action == 11:   # away from enemy
+                        new_pos = [self.selected.x - self.toward_enemy[0], self.selected.y - self.toward_enemy[1]]
+                    
+                    if action == 8 or action == 9:      # move
+                        action_mapped.append(actions.RAW_FUNCTIONS.Move_pt("now", self.selected.tag, new_pos))
+                    elif action == 10 or action == 11:  # attack
+                        action_mapped.append(actions.RAW_FUNCTIONS.Attack_pt("now", self.selected.tag, new_pos))
 
         else:
             print("shit b fky as fuck")
+
+        self.selected = self.marines[self.current_marine_it]
+        action_mapped.append(actions.RAW_FUNCTIONS.Behavior_HoldFireOff_quick("now", self.selected.tag))
 
         raw_obs = self.env.step([action_mapped])[0]
 
